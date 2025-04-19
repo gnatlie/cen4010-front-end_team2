b9d042d7c006114c931d24361fa4d5f2e8de14bc
natalie almonte <nalmo011@fiu.edu>
2025-04-19 13:12:05 -0400

    adding health level lables

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
+"use client";
+
 import dogImage from './img/Dog.png'
 import './css/mainStyles.css';
+import React, { useState, useEffect } from 'react';
+
 
 export default function Home() {
+  const [hungerLabel, setHunger] = useState(80);
+  const [cleanLabel, setClean] = useState(70);
+  const [playLabel, setPlay] = useState(90);
+
+  const updateClean = () => {
+    setClean(100);
+  };
+
+  const updateHunger = () => {
+    setHunger(100);
+  };
+
+  const updatePlay = () => {
+    setPlay(100);
+  };
   return (
     <div className="page_style">
       <div className="btn-group">
         <button>Logout</button>
-        <button>Dog</button>
+        <button>Fluffy</button>
         <button>0:56</button>
       </div>
 
@@ -21,9 +40,15 @@ export default function Home() {
       </div>
 
       <div className="btn-group">
-        <button>Clean</button>
-        <button>Feed</button>
-        <button>Play</button>
+
+        <button onClick={updateClean}>Clean</button>
+        <label id="cleanLevel">{cleanLabel}/100</label>
+
+        <button onClick={updateHunger}>Feed</button>
+        <label id="playLevel">{hungerLabel}/100</label>
+
+        <button onClick={updatePlay}>Play</button>
+        <label id="hungerLevel">{playLabel}/100</label>
       </div>
     </div>
   );
