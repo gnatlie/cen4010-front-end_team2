cea8de75547135c096a9f69aa776fe767a0166fa
natalie almonte <nalmo011@fiu.edu>
2025-04-19 10:28:11 -0400

    finalized front-end designs

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
+import dogImage from './img/Dog.png'
+import './css/mainStyles.css';
+
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
-      <h1>Welcome!</h1>
-      <p>Go to <a href="/login">Login</a></p>
+    <div className="page_style">
+      <div className="btn-group">
+        <button>Logout</button>
+        <button>Dog</button>
+        <button>0:56</button>
+      </div>
+
+      <div className="container">
+        <div className="triangle-left"></div>
+        <div className="oval-wrapper">
+          <div className="oval">
+            <img src={dogImage.src} alt="Puppy" className="oval-img" />
+          </div>
+        </div>
+        <div className="triangle-right"></div>
+      </div>
+
+      <div className="btn-group">
+        <button>Clean</button>
+        <button>Feed</button>
+        <button>Play</button>
+      </div>
     </div>
   );
 }
